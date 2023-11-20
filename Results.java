ab976ea453c11fa3538a52e54634111727c963f7
manojkotni <99539907+manojkotni@users.noreply.github.com>
2023-11-20 19:12:58 +0530

    Add files via upload

---
+++ Results.java
@@ -0,0 +1,34 @@
+package practices;
+import java.util.Scanner;
+
+public class Results {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+        // Get marks for three subjects
+        System.out.print("Enter marks for subject 1: ");
+        int subject1 = scanner.nextInt();
+
+        System.out.print("Enter marks for subject 2: ");
+        int subject2 = scanner.nextInt();
+
+        System.out.print("Enter marks for subject 3: ");
+        int subject3 = scanner.nextInt();
+
+        // Check conditions to declare the result
+        if (subject1 > 60 && subject2 > 60 && subject3 > 60) {
+            System.out.println("Result: Passed");
+        } else if ((subject1 > 60 && subject2 > 60) || (subject2 > 60 && subject3 > 60) || (subject1 > 60 && subject3 > 60)) {
+            System.out.println("Result: Promoted");
+        } else {
+            System.out.println("Result: Failed");
+        }
+
+        // Close the scanner
+        scanner.close();
+
+	}
+
+}
---
+++ TwoString.java
@@ -0,0 +1,17 @@
+package practices;
+
+public class TwoString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "Hello,";
+        String str2 = "How are you?";
+
+        // Use the concat() method to join the strings
+        String result = str1.concat(" ").concat(str2);
+
+        // Display the result
+        System.out.println(result);
+	}
+
+}
